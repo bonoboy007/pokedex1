a03539d08520ad078d18f2ca4dd9d4919c563a87
John Rogers <john.rogers0076@googlemail.com>
2016-05-24 11:56:17 +0100

    Initial Commit

---
+++ Pokedex01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex01
+//
+//  Created by john rogers on 24/05/2016.
+//  Copyright © 2016 john rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
